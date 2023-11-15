b74222b6f4d2a892efc40c9e9a5a79b0b4066428
Udenar <enderfinofino666@gmail.com>
2023-11-15 15:37:28 -0500

    Menu fix commit

--- app/src/main/java/com/example/ar_final_project/MainActivity.kt
+++ app/src/main/java/com/example/ar_final_project/MainActivity.kt
@@ -7,33 +7,36 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavListener)
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         Log.d("TAG", "onCreateOptionsMenu called")
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
+    private val bottomNavListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
             R.id.page_home -> {
                 replaceFragment(Home())
-                true
+                return@OnNavigationItemSelectedListener true
             }
             R.id.page_about -> {
                 replaceFragment(About())
-                true
+                return@OnNavigationItemSelectedListener true
             }
-            else -> true
+            else -> return@OnNavigationItemSelectedListener false
         }
-}
+    }
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
-            .commit() } }
+            .commit()
+    }
+}
