758b728793d77117fd26c0086e9d2dd4ee321b50
Dani <enderfinofino666@gmail.com>
2023-11-25 01:55:08 -0500

    Login request retrofit commit

--- app/src/main/java/com/example/ar_final_project/Login.kt
+++ app/src/main/java/com/example/ar_final_project/Login.kt
@@ -9,11 +9,10 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidUserException
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.auth
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+
 
 class Login : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth;
@@ -23,24 +22,39 @@ class Login : AppCompatActivity() {
         val blogin=findViewById<AppCompatButton>(R.id.blogin)
         val uname=findViewById<EditText>(R.id.username)
         val upass=findViewById<EditText>(R.id.userpass)
-        auth = Firebase.auth
-        blogin.setOnClickListener(){
-            var name=uname.text.toString()
-            var pass=upass.text.toString()
+        blogin.setOnClickListener {
+            var name = uname.text.toString()
+            var pass = upass.text.toString()
             if (name.isNotEmpty() && pass.isNotEmpty()) {
-                auth.signInWithEmailAndPassword(name, pass)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            val intent= Intent(this,MainActivity::class.java)
-                            startActivity(intent)
-                            val user: FirebaseUser? = auth.currentUser
-                        } else {
-                            val text = "Error en inicio de sesión"
-                            val duration = Toast.LENGTH_SHORT
-                            val toast = Toast.makeText(this, text, duration)
-                            toast.show()
+                val service = RetrofitService.RetrofitServiceFactory.makeRetrofitService()
+                lifecycleScope.launch {
+                    try {
+                        val response = service.login(name, pass)
+                            // Successful login (HTTP 2xx)
+                        response?.let {
+                            if (response.str == "success") {
+                                // Navigate to the next activity upon successful login
+                                val intent = Intent(this@Login, MainActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                // Handle other cases based on the response content
+                                Toast.makeText(
+                                    this@Login,
+                                    "Login failed: ${response.str}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
                         }
+                    } catch (e: Exception) {
+                        // Handle network or other exceptions
+                        Toast.makeText(
+                            this@Login,
+                            "Login failed: ${e.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
+                }
             }
         }
     }
